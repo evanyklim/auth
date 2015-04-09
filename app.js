b928f70a603b07ab0252a90afd9d727afd93c5f0
Evan Lim <evanlim@Evan-Lims-MacBook-Pro.local>
2015-04-09 16:00:03 -0400

    completed post to signup

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var path = require('path');
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
 var routes = require('./server');
 
-app.get('/', routes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use('/', routes);
 
 app.listen(3000, function () {
 	console.log('the server is now initialized and listening on port 3000');
--- server/index.js
+++ server/index.js
@@ -3,10 +3,21 @@ var router = require('express').Router();
 var models = require('../models');
 var user = models.userProfile;
 var indexHtmlPath = path.join(__dirname, '../templates/index.html');
+var signupHtmlPath = path.join(__dirname, '../templates/signup.html');
 
 router.get('/', function (req, res, next) {
 
 	res.sendFile(indexHtmlPath);
 });
+
+router.get('/signup', function (req, res, next) {
+
+	res.sendFile(signupHtmlPath);
+});
+
+router.post('/signup', function (req, res, next) {
+
+	console.log(req.body);
+})
 
 module.exports = router;

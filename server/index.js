428b016e91403a23c910e39ed6490b80247c7acb
Evan Lim <evanlim@Evan-Lims-MacBook-Pro.local>
2015-04-10 11:56:07 -0400

    added hashing functions

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 var path = require('path');
 var router = require('express').Router();
 var userProfile = require('../models');
+var crypto = require('crypto');
 
 var indexHtmlPath = path.join(__dirname, '../templates/index.html');
 var signupHtmlPath = path.join(__dirname, '../templates/signup.html');
 var loginHtmlPath = path.join(__dirname, '../templates/login.html');
 var successHtmlPath = path.join(__dirname, '../templates/success.html');
 var failureHtmlPath = path.join(__dirname, '../templates/failure.html');
+
+function hashAway (pwdByUser) {
+	return crypto.pbkdf2Sync(pwdByUser, '', 0, 16).toString('base64');
+}
 
 router.get('/', function (req, res, next) {
 
@@ -19,7 +24,13 @@ router.get('/signup', function (req, res, next) {
 });
 
 router.post('/signup', function (req, res, next) {	
-	userProfile.create(req.body, function(err, user) {
+
+    var userObj = {
+    	username: req.body.username,
+    	password: hashAway(req.body.password)
+    };
+
+	userProfile.create(userObj, function(err, user) {
 		if (err) return next(err);
 
 		res.redirect('/success');
@@ -32,10 +43,17 @@ router.get('/login', function (req, res, next) {
 });
 
 router.post('/login', function (req, res, next) {
-	userProfile.findOne( req.body , function(err, user) {
+	userProfile.findOne( { username: req.body.username } , function(err, user) {
 		if (err) return next(err);
 		if (!user) { res.redirect('/failure'); }
-		else { res.redirect('/success'); }
+		else {
+
+			if (hashAway(req.body.password) === user.password) {
+				res.redirect('/success'); 
+			} else {
+				res.redirect('/failure');
+			}
+		}
 	});
 });
 
